add1b46a324730edf04b37229ff3da293edb9405
Crowdbotics <engineering@crowdbotics.com>
2023-02-06 00:19:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/billowing_union_dev_61356/MainActivity.java
@@ -0,0 +1,15 @@
+package com.billowing_union_dev_61356;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "billowing_union_dev_61356";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-union-dev-61356",
+  url: "https://billowing-union-dev-61356.botics.co",
+  api: "https://billowing-union-dev-61356.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
